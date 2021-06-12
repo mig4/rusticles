21a3676feaa2c71bbf0aeccccc899b2dd5543c8e
mig4 <42650719@auril.club>
2021-06-12 23:54:45 +0100

    the-book: 2-guessing-game: gen random number

--- the-book-excercises/guessing-game/src/main.rs
+++ the-book-excercises/guessing-game/src/main.rs
@@ -1,6 +1,10 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("secret: {}", secret_number);
+
     println!("Guess the number!");
 
     println!("Please input your guess.");
