1623764e87a753e78e68013faa5d4db41a808cdc
mig4 <42650719@auril.club>
2021-06-13 00:11:32 +0100

    the-book: 2-guessing-game: compare the numbers

--- the-book-excercises/guessing-game/src/main.rs
+++ the-book-excercises/guessing-game/src/main.rs
@@ -1,5 +1,6 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..101);
@@ -15,5 +16,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: i32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
