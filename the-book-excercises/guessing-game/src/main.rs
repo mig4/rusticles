8743e47124d7503c1a02fdaad2e026a4a7bbcdfe
mig4 <42650719@auril.club>
2021-06-13 13:40:47 +0100

    the-book: 2-guessing-game: loop & handle errors
    Also respond to `quit` command to stop the game.

--- the-book-excercises/guessing-game/src/main.rs
+++ the-book-excercises/guessing-game/src/main.rs
@@ -4,25 +4,41 @@ use std::io;
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..101);
-    println!("secret: {}", secret_number);
 
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: i32 = guess.trim().parse().expect("Please type a number!");
+        let guess = guess.trim();
 
-    println!("You guessed: {}", guess);
+        if guess.eq_ignore_ascii_case("quit") {
+            break
+        }
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        let guess: i32 = match guess.parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number! (got: {})", guess);
+                continue
+            }
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
