51b7d8106caf6e1d26cf4cf9a8e69f20f12c41aa
mig4 <42650719@auril.club>
2021-06-30 22:33:12 +0100

    rust-cli-book/grrs: first implementation

--- rust-cli-book/grrs/src/main.rs
+++ rust-cli-book/grrs/src/main.rs
@@ -14,5 +14,12 @@ struct Cli {
 
 fn main() {
     let args = Cli::from_args();
-    println!("args: {:?}", args);
+    let content = std::fs::read_to_string(&args.path)
+        .expect("Could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
