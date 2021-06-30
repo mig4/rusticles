1aaa8f45b767657f5d7705eef441a7cae26a7d30
mig4 <42650719@auril.club>
2021-06-30 22:24:32 +0100

    rust-cli-book/grrs: hello world with structopt

--- rust-cli-book/grrs/src/main.rs
+++ rust-cli-book/grrs/src/main.rs
@@ -1,3 +1,18 @@
+use structopt::StructOpt;
+
+/// Search for pattern in a file and display the lines that contain it.
+#[derive(Debug)]
+#[derive(StructOpt)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    println!("args: {:?}", args);
 }
