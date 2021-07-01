9d3517b146ce19e8de83a529691f5158869b54e4
mig4 <42650719@auril.club>
2021-07-01 13:42:57 +0100

    rust-cli-book/grrs: nicer error reporting
    using `anyhow` library which allows chaining errors to attach additional
    context and make it clearer what caused a failure.

--- rust-cli-book/grrs/src/main.rs
+++ rust-cli-book/grrs/src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context, Result};
 use std::io::BufReader;
 use std::io::prelude::*;
 use std::fs::File;
@@ -15,16 +16,18 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::from_args();
     let f = File::open(&args.path)
-        .expect("Could not open file");
+        .with_context(|| format!("Could not open file {:?}", &args.path))?;
     let f = BufReader::new(f);
 
     for line in f.lines() {
-        let line = line.unwrap();
+        let line = line.context("Could not read line")?;
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
+
+    Ok(())
 }
