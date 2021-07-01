a9530b0e3f005dfe7c8cad8de7749ee86dba8527
mig4 <42650719@auril.club>
2021-07-01 20:02:54 +0100

    rust-cli-book/grrs: testing / making code testable
    Split the core piece of the application to make it more flexible and
    testable, and add a unit test for it.

--- rust-cli-book/grrs/src/main.rs
+++ rust-cli-book/grrs/src/main.rs
@@ -22,12 +22,27 @@ fn main() -> Result<()> {
         .with_context(|| format!("Could not open file {:?}", &args.path))?;
     let f = BufReader::new(f);
 
-    for line in f.lines() {
+    find_matches(f, &args.pattern, &mut std::io::stdout())
+}
+
+fn find_matches(content: impl BufRead, pattern: &str, mut writer: impl Write) -> Result<()> {
+    for line in content.lines() {
         let line = line.context("Could not read line")?;
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line).context("Could not output line")?;
         }
     }
-
     Ok(())
+}
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+
+    find_matches(
+        std::io::Cursor::new("lorem ipsum\ndolor sit amet"),
+        "lorem",
+        &mut result,
+    ).expect("find_matches failed unexpectedly");
+    assert_eq!(result, b"lorem ipsum\n");
 }
