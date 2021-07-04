cf4e70100da4155478210117a656a20ef0f44063
mig4 <42650719@auril.club>
2021-07-04 10:44:41 +0100

    the-book: 7-restaurant: further split module files
    Split `front_of_house` itself into separate module file for each
    submodule.

--- the-book-excercises/restaurant/src/front_of_house.rs
+++ the-book-excercises/restaurant/src/front_of_house.rs
@@ -1,13 +1,3 @@
-pub mod hosting {
-    pub fn add_to_waitlist() {}
+pub mod hosting;
 
-    fn seat_at_table() {}
-}
-
-mod serving {
-    fn take_order() {}
-
-    fn serve_order() {}
-
-    fn take_payment() {}
-}
+mod serving;
