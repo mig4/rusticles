a07aebb3f32549f67d58c1c9044414de8d6fec31
mig4 <42650719@auril.club>
2021-07-04 10:33:54 +0100

    the-book: 7-restaurant: defining modules
    + using paths and bringing them into scope.

--- the-book-excercises/restaurant/src/lib.rs
+++ the-book-excercises/restaurant/src/lib.rs
@@ -1,3 +1,64 @@
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    // imported path
+    hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("rye");
+    // Change our mind about the bread we'd like
+    meal.toast = String::from("wheat");
+    println!("I'd like {} toast please", meal.toast);
+    // won't work as it's private
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Salad;
+    let order2 = back_of_house::Appetizer::Soup;
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
