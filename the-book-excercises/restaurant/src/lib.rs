6089ed7ea074cae0798904edfe6e0f5570e8d775
mig4 <42650719@auril.club>
2021-07-04 10:41:45 +0100

    the-book: 7-restaurant: split module files
    Split `front_of_house` into separate module file.

--- the-book-excercises/restaurant/src/lib.rs
+++ the-book-excercises/restaurant/src/lib.rs
@@ -19,21 +19,7 @@ mod back_of_house {
     }
 }
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
 
 use crate::front_of_house::hosting;
 
